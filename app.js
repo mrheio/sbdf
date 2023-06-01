ed519af86d3996bf05c19526d94f8d4232415597
dragoio <ovidiu.dragoi@haufe.com>
2023-06-01 12:23:53 +0300

    init express server

---
+++ app.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`APP listening on port ${port}`);
+});
